297f6a6f31c33de5864bb914ee2598ceff51b65c
Jamoliddin <100786889+Jamoliddin0710@users.noreply.github.com>
2024-09-30 19:39:16 +0500

    Initial commit

---
+++ PartialView.Example/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialView.Example.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Countries";
+        ViewData["ListItems"] = new List<string>()
+        {
+            "Spain",
+            "Germany",
+            "USA",
+            "UAE"
+        };
+        
+        return View();
+    }
+
+    [Route("/about")]
+    public IActionResult About()
+    {
+        return View();
+    }
+    
+    // RenderPartialView bilan PartialView farqi RenderPartialView void qaytaradi u bajarilganda ma'lumotlar 
+    // stream holatida browserga saqlanadi.
+    // Partial da esa ma'lumot IHtmlContent qaytaradi undan qayta foydalanish ham mumkin
+}
